5db747b6a45e535c2878f85912a0083ca0792e01
carlos <[cmat27@hotmail.co]>
2019-01-13 01:46:45 -0500

    fullstack- first commint

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    entry: './src/index.js',
+    output: {
+        path: __dirname+'/public',
+        filename: 'bundle.js'
+    },
+    module:{
+        loaders:[{
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
+    }
+};
